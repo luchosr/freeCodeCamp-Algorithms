e9e40dd8f1f57303e472644cb5d5606d2c9e8d3b
Luciano <luchosr@gmail.com>
2017-10-31 21:29:48 -0300

    Add some new algorihms

---
+++ Basic Algorithm Scripting/2_Factoralize_a_number.js
@@ -0,0 +1,33 @@
+/* 
+Return the factorial of the provided integer.
+
+If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+
+Factorials are often represented with the shorthand notation n!
+
+For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+
+function factorialize(num) {
+  return num;
+}
+
+factorialize(5);
+*/
+
+// jshint esversion:6
+factorialize = num => {
+  let acumulador = 1;
+  for (let i = num; i > 1; i--) {
+    acumulador = acumulador * i;
+  }
+  return acumulador;
+};
+
+factorialize(5);
+
+/*factorialize(5) should return a number.
+factorialize(5) should return 120.
+factorialize(10) should return 3628800.
+factorialize(20) should return 2432902008176640000.
+factorialize(0) should return 1.*/
